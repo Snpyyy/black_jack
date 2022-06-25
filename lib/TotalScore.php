923c8eb5d4c1b7543f988c33d9f0a2636c28aba2
Snpyyy <snpyyy.a@gmail.com>
2022-06-25 07:54:10 +0000

    Ace score 1 or 11

--- lib/TotalScore.php
+++ lib/TotalScore.php
@@ -13,17 +13,29 @@ class TotalScore
      */
     private $score = 0;
 
+    private const MAX_ACE_SCORE = 11;
+    private const MIN_ACE_SCORE = 1;
+
     /**
      * @param array<string> $handCards
      */
     public function getScore(array $handCards): int
     {
+        $hand = [];
         $this->score = 0;
         foreach ($handCards as $handCard) {
             $card = new Card(substr($handCard, 0, 1), substr($handCard, 1, strlen($handCard) - 1));
+            $hand[] = $card->getNum();
             $this->score += $card->getNumScore();
         }
 
+        $aceCount = count(array_keys($hand, 'A'));
+        for ($i = 0; $i < $aceCount; $i++) {
+            if (in_array('A', $hand) && $this->score > 21) {
+                $this->score -= self::MAX_ACE_SCORE;
+                $this->score += self::MIN_ACE_SCORE;
+            }
+        }
         return $this->score;
     }
 }
